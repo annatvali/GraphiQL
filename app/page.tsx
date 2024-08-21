16feeb41b3c53e7bb59ffa52f83b54579223c7e2
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-21 18:31:48 +0600

    feat: add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './globals.css';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>REST/GraphiQL Client</h1>
-    </main>
+    <div className="flex flex-col max-w-screen-xl px-4 mx-auto min-h-screen">
+      <Header />
+      <main className="main">
+        <h1 className="title">Welcome to Our API Client</h1>
+        <p>A powerful tool for testing and interacting with your APIs.</p>
+        <div className="buttons buttons_main">
+          <button className="button sign-in">Sign In</button>
+          <button className="button sign-up">Sign Up</button>
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
