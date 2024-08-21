81f323c0b8746f65f268094ce7c696d807ac2010
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-21 18:31:48 +0600

    feat: add header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header className="flex justify-between items-center w-full h-[70px] border-b-2 border-white px-2.5">
+      <div>API Nexus</div>
+      <div className="flex gap-4 items-center">
+        <button className="bg-[rgba(255,255,255,0.3)] hover:bg-[rgba(255,255,255,0.4)] active:shadow-none text-[white] shadow-[0px_0px_20px_-6px_rgba(255,255,255,0.82)] font-light text-xl p-2.5 rounded-[20px]">
+          En
+        </button>
+        <button className="bg-[rgba(255,255,255,0.3)] hover:bg-[rgba(255,255,255,0.4)] active:shadow-none text-[white] shadow-[0px_0px_20px_-6px_rgba(255,255,255,0.82)] font-light text-xl p-2.5 rounded-[20px]">
+          Sign In
+        </button>
+        <button className="bg-[rgba(255,255,255,0.3)] hover:bg-[rgba(255,255,255,0.4)] active:shadow-none text-[white] shadow-[0px_0px_20px_-6px_rgba(255,255,255,0.82)] font-light text-xl p-2.5 rounded-[20px]">
+          Sign Up
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
