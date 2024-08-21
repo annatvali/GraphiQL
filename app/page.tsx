2b770f5f8073c3c092751751267647beb747cf92
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-21 19:03:42 +0600

    fix: main component

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,9 @@ export default function Home() {
   return (
     <div className="flex flex-col max-w-screen-xl px-4 mx-auto min-h-screen">
       <Header />
-      <main className="main">
-        <h1 className="title">Welcome to Our API Client</h1>
+      <main className="flex flex-col p-2.5 flex-1">
+        <h1 className="color-white text-[45px] font-medium">Welcome to Our API Client</h1>
         <p>A powerful tool for testing and interacting with your APIs.</p>
-        <div className="buttons buttons_main">
-          <button className="button sign-in">Sign In</button>
-          <button className="button sign-up">Sign Up</button>
-        </div>
       </main>
       <Footer />
     </div>
