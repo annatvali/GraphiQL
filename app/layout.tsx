ec4bb5da768ffd4db8ff434b8dc043447fcecf10
annatvali <anntvaliashvili@gmail.com>
2024-08-22 23:01:27 +0400

    chore: add Header component in app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
+import Header from './components/Header';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata: Metadata = {
   title: 'REST/GraphiQL Client',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={openSans.className}>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
