768fb599a4fa46af4827f04e5d75d05ead9214d1
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-28 00:43:12 +0600

    feat: add welcome page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      width: {
+        '250': '250px',
+        '300': '300px',
+        '320': '320px',
+      },
+      height: {
+        '300': '300px',
+        '320': '320px',
+        '600': '600px',
+      },
+      minHeight: {
+        '600': '600px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundSize: {
+        'custom-size': '850px 450px',
+      },
       colors: {
         'custom-gray': 'rgb(215, 215, 215)',
+        'custom-light-gray': 'rgb(183, 183, 183)',
+        'custom-purple': 'rgb(186, 128, 217)',
       },
       maxWidth: {
         '1440px': '1440px',
+        '360': '360px',
       },
     },
   },
