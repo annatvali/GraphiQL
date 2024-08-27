ce9bea2fecf8605ea1dae858cea6b90d22866483
Alexandra Paramonova <alpar.wb@gmail.com>
2024-08-27 21:28:59 +0500

    feat: add LanguageChanger component

---
+++ app/components/LanguageChanger.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useRouter, usePathname } from '@/navigation';
+import { Locale, localeNames, locales } from '@/i18n.config';
+import { Dropdown, DropdownItem } from './Dropdown';
+
+interface LanguageChangerProps {
+  locale: Locale;
+}
+
+const LanguageChanger = ({ locale }: LanguageChangerProps): React.ReactNode => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleChange = (newLocale: Locale) => {
+    router.push(pathname, { locale: newLocale });
+  };
+
+  return (
+    <Dropdown initialValue={locale} className="capitalize">
+      {locales.map((localeItem) => (
+        <DropdownItem key={localeItem} label={localeNames[localeItem]} onClick={() => handleChange(localeItem)} />
+      ))}
+    </Dropdown>
+  );
+};
+
+export default LanguageChanger;
