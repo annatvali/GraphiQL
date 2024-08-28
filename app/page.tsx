a4c8b4c0d87eed1dfda0b5ab7139a13052ee2b02
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-29 00:02:52 +0600

    feat: add a separate TeamMemberCardcomponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import React from 'react';
 import Image from 'next/image';
-export default function Home() {
+import TeamMemberCard from './components/TeamMemberCard';
+import { teamMembers } from './data/teamMembers';
+
+const Home: React.FC = () => {
   return (
     <div className="flex flex-col max-w-screen-xl px-4 mx-auto mt-10">
       <section className="flex flex-col min-h-600 mt-10 bg-[url('../public/cloud.png')] bg-no-repeat bg-custom-size bg-right sm-max:bg-[url('../public/lins.png')]">
@@ -27,51 +31,9 @@ export default function Home() {
       <section className="mb-8">
         <h2 className="text-[65px] text-center font-semibold">Our team</h2>
         <ul className="flex flex-wrap justify-around gap-10 list-nonec mt-10">
-          <li className="flex flex-col items-center w-320">
-            <div className="p-3 bg-white">
-              <Image src={'/valeriya.jpg'} width={400} height={350} alt="Valeriya" />
-            </div>
-            <div className="bg-white -mt-8 w-250 text-custom-purple text-center font-semibold p-3">
-              <h3 className="text-[35px] font-semibold">Valeriya Tikhonova</h3>
-              <p className="my-5 text-custom-light-gray">Front-end Developer</p>
-              <p>
-                My name is Valeria and I am a beginner frontend developer who strives to create beautiful and functional
-                web applications. I am passionate about web development and constantly develop my skills in working with
-                modern technologies. At the moment, I am actively studying: HTML, CSS, JavaScript and React. I am always
-                open to new challenges and opportunities to grow as a developer and bring value to the team.
-              </p>
-            </div>
-          </li>
-          <li className="flex flex-col items-center w-320">
-            <div className="p-3 bg-white">
-              <Image src={'/rs-school-img.jpg'} width={300} height={100} alt="Ana" />
-            </div>
-            <div className="bg-white -mt-8 w-250 text-custom-purple text-center font-semibold p-3">
-              <h3 className="text-[35px] font-semibold">Ana Tvaliashvili</h3>
-              <p className="my-5 text-custom-light-gray">Team Lead / Front-end Developer</p>
-              <p>
-                Greetings! I am a motivated tech enthusiast transitioning into a career in technology, currently
-                enrolled in courses at RSSchool in front-end development and AWS Cloud Developer training. With a strong
-                commitment to self-development, I am eager to leverage my skills and knowledge in tech to drive
-                innovative solutions and contribute to impactful projects.
-              </p>
-            </div>
-          </li>
-          <li className="flex flex-col items-center w-320">
-            <div className="p-3 bg-white">
-              <Image src={'/rs-school-img.jpg'} width={300} height={300} alt="Alexandra" />
-            </div>
-            <div className="bg-white -mt-8 w-250 text-custom-purple text-center font-semibold p-3">
-              <h3 className="text-[35px] font-semibold">Alexandra Paramonova</h3>
-              <p className="my-5 text-custom-light-gray">Front-end Developer</p>
-              <p>
-                Hi! I am a beginner front-end developer with a keen interest in building engaging and user-friendly web
-                applications. I&apos;m excited to apply my skills to real-world projects, collaborate with others and
-                take on new challenges in the field. I&apos;m grateful for the opportunity to learn from RS School,
-                which has given me a strong foundation in HTML, CSS, and JavaScript!
-              </p>
-            </div>
-          </li>
+          {teamMembers.map((elem, index) => (
+            <TeamMemberCard key={index} member={elem} />
+          ))}
         </ul>
       </section>
       <section className="flex flex-col min-h-600 mt-8 pb-10">
@@ -91,4 +53,6 @@ export default function Home() {
       </section>
     </div>
   );
-}
+};
+
+export default Home;
