3154b8c165f3313111f853470501bd57736f8490
annatvali <anntvaliashvili@gmail.com>
2024-09-03 00:55:40 +0400

    feat: implement sign up form for login route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata: Metadata = {
   title: 'REST/GraphiQL Client',
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={openSans.className}>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
