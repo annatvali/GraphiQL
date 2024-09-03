e4d7bb0cfd450762e353e9ded508adfd9222e2c7
annatvali <anntvaliashvili@gmail.com>
2024-09-04 00:50:19 +0400

    fix: add missing href attribute to Buttons on UnauthenticatedPage

--- app/components/UnauthenticatedPage.tsx
+++ app/components/UnauthenticatedPage.tsx
@@ -7,8 +7,8 @@ const UnauthenticatedPage: React.FC = () => {
       <h1 className="color-white text-6xl font-medium">Welcome to API Nexus</h1>
       <p className="my-2 text-lg">A powerful tool for testing and interacting with your APIs.</p>
       <div className="flex gap-4 my-8">
-        <Button>Sign In</Button>
-        <Button>Sign Up</Button>
+        <Button href={'/login'}>Sign In</Button>
+        <Button href={'/register'}>Sign Up</Button>
       </div>
     </section>
   );
