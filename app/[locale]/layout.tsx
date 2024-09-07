37ccb9f1e3310d010cf3c3891f09545b8b4f2194
Alexandra Paramonova <alpar.wb@gmail.com>
2024-09-07 12:58:27 +0500

    fix: add locale layout for i18n

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 import Header from '@/app/components/Header';
 import Footer from '@/app/components/Footer';
 import '@/app/globals.css';
@@ -11,17 +13,23 @@ export const metadata: Metadata = {
   description: 'A lightweight REST/GraphQL client for seamless API interactions',
 };
 
-export default function RootLayout({
+export default async function LocaleLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  const messages = await getMessages();
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={openSans.className}>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <NextIntlClientProvider messages={messages}>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
