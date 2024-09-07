462a2fba9ad8728a3f2f7ec7de7a9d82e83ead85
Alexandra Paramonova <alpar.wb@gmail.com>
2024-09-07 19:42:14 +0500

    fix: correct pages height display

--- app/[locale]/graphiql-client/page.tsx
+++ app/[locale]/graphiql-client/page.tsx
@@ -5,7 +5,7 @@ const GraphiQLClientPage: React.FC = () => {
   const t = useTranslations('GRAPHI_CLIENT');
 
   return (
-    <div className="flex flex-col max-w-screen-xl px-4 mx-auto h-[80vh] mt-40">
+    <div className="flex flex-col max-w-screen-xl px-4 mx-auto mt-40">
       <h1 className="color-white text-6xl font-medium">{t('title')}</h1>
       <p>Content for GraphiQL Client goes here.</p>
     </div>
--- app/[locale]/rest-client/page.tsx
+++ app/[locale]/rest-client/page.tsx
@@ -5,7 +5,7 @@ const RestClientPage: React.FC = () => {
   const t = useTranslations('REST_CLIENT');
 
   return (
-    <div className="flex flex-col max-w-screen-xl px-4 mx-auto min-h-[80vh] mt-40">
+    <div className="flex flex-col max-w-screen-xl px-4 mx-auto mt-40">
       <h1 className="color-white text-6xl font-medium">{t('title')}</h1>
       <p>Content for REST Client goes here.</p>
     </div>
