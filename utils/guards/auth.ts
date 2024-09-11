68811d208ad8cef7f502712ee356c0e60354989b
Alexandra Paramonova <alpar.wb@gmail.com>
2024-09-11 19:01:31 +0500

    chore: add guards for auth types

---
+++ utils/guards/auth.ts
@@ -0,0 +1,55 @@
+import { AuthError, AuthUser, SignInResponse, SignInSuccessData, SignOutResponse, SignOutSuccessData } from '@/types';
+
+export const isAuthError = (error: unknown): error is AuthError => {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'code' in error &&
+    typeof error.code === 'string' &&
+    'message' in error &&
+    typeof error.message === 'string'
+  );
+};
+
+export const isAuthUser = (user: unknown): user is AuthUser => {
+  return (
+    typeof user === 'object' &&
+    user !== null &&
+    'uid' in user &&
+    typeof user.uid === 'string' &&
+    'email' in user &&
+    (user.email === null || typeof user.email === 'string') &&
+    'displayName' in user &&
+    (user.displayName === null || typeof user.displayName === 'string')
+  );
+};
+
+const isSignInSuccessData = (data: unknown): data is SignInSuccessData => {
+  return typeof data === 'object' && data !== null && 'user' in data && isAuthUser(data.user);
+};
+
+const isSignOutSuccessData = (data: unknown): data is SignOutSuccessData => {
+  return typeof data === 'object' && data !== null && 'isSignedOut' in data && data.isSignedOut === 'boolean';
+};
+
+export const isSignInResponse = (response: unknown): response is SignInResponse => {
+  return (
+    typeof response === 'object' &&
+    response !== null &&
+    'data' in response &&
+    'error' in response &&
+    ((response.error === null && isSignInSuccessData(response.data)) ||
+      (response.data === null && isAuthError(response.error)))
+  );
+};
+
+export const isSignOutResponse = (response: unknown): response is SignOutResponse => {
+  return (
+    typeof response === 'object' &&
+    response !== null &&
+    'data' in response &&
+    'error' in response &&
+    ((response.error === null && isSignOutSuccessData(response.data)) ||
+      (response.data === null && isAuthError(response.error)))
+  );
+};
