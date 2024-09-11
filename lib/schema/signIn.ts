21217cfab6de4cb0688dceb8603793b7d131f931
Alexandra Paramonova <alpar.wb@gmail.com>
2024-09-11 22:19:07 +0500

    fix: correct regex to check special character in validations

--- lib/schema/signIn.ts
+++ lib/schema/signIn.ts
@@ -12,7 +12,7 @@ export const signInSchema = (t: ReturnType<typeof useTranslations<'VALIDATION'>>
       .min(8, { message: t('password-min-length') })
       .regex(/\p{L}/u, { message: t('password-must-contain-letter') })
       .regex(/\p{N}/u, { message: t('password-must-contain-digit') })
-      .regex(/\P{L}\P{N}/u, { message: t('password-must-contain-special') })
+      .regex(/[ `!@#$%^&*()_+\-=\\[\]{};':"\\|,.<>/?~]/u, { message: t('password-must-contain-special') })
       .trim(),
   });
 
