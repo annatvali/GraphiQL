674aa467155c93852b78bd97d7a6dd4f549f3585
Alexandra Paramonova <alpar.wb@gmail.com>
2024-09-13 08:17:54 +0500

    fix: correct sign up form to use only username

--- app/[locale]/register/page.tsx
+++ app/[locale]/register/page.tsx
@@ -49,19 +49,11 @@ const Register = () => {
       onSubmit={handleSubmit}
     >
       <FormField
-        label={t('fname_label')}
+        label={t('username_label')}
         type="text"
-        name="first-name"
-        id="first-name"
-        placeholder={t('fname_label')}
-        required
-      />
-      <FormField
-        label={t('lname_label')}
-        type="text"
-        name="last-name"
-        id="last-name"
-        placeholder={t('lname_label')}
+        name="username"
+        id="username"
+        placeholder={t('username_label')}
         required
       />
       <FormField
